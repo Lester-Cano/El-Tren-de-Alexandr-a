2644f94b9ba141f094af4a95fadc1a15d6499e2d
Nicolás <skarddy@gmail.com>
2022-08-21 16:25:49 -0500

    Main Menu code
    Created Main menu with placeholders and DOTween code

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+    [SerializeField] GameObject controlsP, mainMenuP, optionsP, MPosition, RPosition, LPosition;
+
+    private void Start() {
+        DOTween.Init();
+    }
+    public void OnControlsButton() {
+        controlsP.transform.DOMove(MPosition.gameObject.transform.position, 0.5f);
+        mainMenuP.transform.DOMove(LPosition.gameObject.transform.position, 0.5f);
+    }
+    public void OnOptionsButton() {
+        optionsP.transform.DOMove(MPosition.gameObject.transform.position, 0.5f);
+        mainMenuP.transform.DOMove(RPosition.gameObject.transform.position, 0.5f);  
+    }
+
+    public void OnReturnButton() {
+        mainMenuP.transform.DOMove(MPosition.gameObject.transform.position, 0.5f);
+        controlsP.transform.DOMove(RPosition.gameObject.transform.position, 0.5f);
+        optionsP.transform.DOMove(LPosition.gameObject.transform.position, 0.5f);
+    }
+    public void OnPlayButton() {
+        SceneManager.LoadScene("1");
+    }
+
+    public void OnExitButton() {
+        Application.Quit();
+    }
+}
