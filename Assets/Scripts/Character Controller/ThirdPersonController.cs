a4be6ed3f99ad0bfccb650b11c2eeed1412354bf
Néstor <nestcano2971@gmail.com>
2022-08-26 22:59:22 -0500

    Beginning character controller

---
+++ Assets/Scripts/Character Controller/ThirdPersonController.cs
@@ -0,0 +1,106 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ThirdPersonController : MonoBehaviour
+{
+    //Input area
+    private ThirdPersonActionsAssets playerActionsAssets;
+    private InputAction move;
+
+    //movement area
+    private Rigidbody rb;
+    [SerializeField] private float movementForce = 1f;
+    [SerializeField] private float maxSpeed = 5f;
+
+    private Vector3 forceDirection = Vector3.zero;
+
+    [SerializeField] private Camera playerCamera;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        playerActionsAssets = new ThirdPersonActionsAssets();
+    }
+
+    private void OnEnable()
+    {
+        move = playerActionsAssets.Player.Move;
+        playerActionsAssets.Player.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerActionsAssets.Player.Disable();
+    }
+
+    private void FixedUpdate()
+    {
+        forceDirection += move.ReadValue<Vector2>().x * GetCameraRight(playerCamera) * movementForce;
+        forceDirection += move.ReadValue<Vector2>().y * GetCameraForward(playerCamera) * movementForce;
+
+        rb.AddForce(forceDirection, ForceMode.Impulse);
+        forceDirection = Vector3.zero;
+
+        if(rb.velocity.y < 0f)
+        {
+            rb.velocity += Vector3.down * Physics.gravity.y * Time.fixedDeltaTime;
+        }
+
+        Vector3 horizontalVelocity = rb.velocity;
+        horizontalVelocity.y = 0;
+
+        if (horizontalVelocity.sqrMagnitude > maxSpeed * maxSpeed)
+        {
+            rb.velocity = horizontalVelocity.normalized * maxSpeed + Vector3.up * rb.velocity.y;
+        }
+
+        LookAt();
+
+    }
+
+    private void LookAt()
+    {
+        Vector3 direction = rb.velocity;
+        direction.y = 0f;
+
+        if (move.ReadValue<Vector2>().sqrMagnitude > 0.1f && direction.sqrMagnitude > 0.1f)
+        {
+            this.rb.rotation = Quaternion.LookRotation(direction, Vector3.up);
+        }
+        else
+        {
+            rb.angularVelocity = Vector3.zero;
+        }
+    }
+
+    private Vector3 GetCameraForward(Camera playerCamera)
+    {
+        Vector3 forward = playerCamera.transform.forward;
+        forward.y = 0;
+        return forward.normalized;
+    }
+
+    private Vector3 GetCameraRight(Camera playerCamera)
+    {
+        Vector3 right = playerCamera.transform.right;
+        right.y = 0;
+        return right.normalized;
+    }
+
+    private bool IsGrounded()
+    {
+        Ray ray = new Ray(transform.position + Vector3.up * 0.25f, Vector3.down);
+        
+        if(Physics.Raycast(ray,out RaycastHit hit,0.3f))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+}
