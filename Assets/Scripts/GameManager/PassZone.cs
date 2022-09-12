78ebe9a1d93bdc8667129a65239f6b767311d33c
Julian Rosero <julian.rosero@outlook.es>
2022-09-11 19:26:03 -0500

    Teleporters - part 1

---
+++ Assets/Scripts/GameManager/PassZone.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PassZone : MonoBehaviour
+{
+    [SerializeField] private GameObject pos;
+    private GameObject objectToMove;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        objectToMove = other.gameObject;
+        StartCoroutine(Teleport());
+    }
+
+    private IEnumerator Teleport()
+    {
+        if (objectToMove != null)
+        {
+            pos.SetActive(false);
+            objectToMove.transform.position = pos.transform.position;
+
+            yield return new WaitForSeconds(2f);
+
+            pos.SetActive(true);
+        }
+    }
+}
