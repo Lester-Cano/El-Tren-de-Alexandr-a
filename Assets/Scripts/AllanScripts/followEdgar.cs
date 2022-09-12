d96c24c07d8d9be6e238acebd81d2f35c8cc278c
Alonso <almear01@gmail.com>
2022-09-12 15:44:06 -0500

    Edgar follows even on tp

--- Assets/Scripts/AllanScripts/followEdgar.cs
+++ Assets/Scripts/AllanScripts/followEdgar.cs
@@ -6,16 +6,30 @@ using UnityEngine.AI;
 public class followEdgar : MonoBehaviour
 {
     // Start is called before the first frame updat
-    [SerializeField] Transform edgar;
+    Transform myTransform;
+    private Vector3 distanceBetween;
+    [SerializeField] float distanceForTp;
+    [SerializeField] Transform edgar,telePos;
     NavMeshAgent nav;
     void Start()
     {
         nav = GetComponent<NavMeshAgent>();
+        myTransform = GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
         nav.SetDestination(edgar.position);
+        distanceBetween = edgar.position - myTransform.position;
+        if (distanceBetween.magnitude > distanceForTp)
+        {
+            tpToEdgar();
+        }
+    }
+
+    public void tpToEdgar()
+    {
+        myTransform.position = telePos.position;
     }
 }
