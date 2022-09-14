20ef10f5ecfcc4b64de466c66df87dbcef6d95a1
Julian Rosero <julian.rosero@outlook.es>
2022-09-13 19:18:37 -0500

    Talk with event system

--- Assets/Scripts/Character Controller/Talk/NPCDialogue.cs
+++ Assets/Scripts/Character Controller/Talk/NPCDialogue.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NPCDialogue: MonoBehaviour
+public class NPCDialogue : MonoBehaviour
 {
     public string[] names;
     public string[] dialogues;
--- Assets/Scripts/GameManager/GameManager.cs
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -8,6 +8,8 @@ public class GameManager : MonoBehaviour
     [SerializeField] PuzzleSolved puzzleSolved;
 
     [SerializeField] private List<GameObject> doors;
+    [SerializeField] private List<GameObject> currentNPC;
+    [SerializeField] private List<GameObject> afterEventNPC;
 
     private void OnEnable()
     {
@@ -22,5 +24,16 @@ public class GameManager : MonoBehaviour
     public void OpenDoor(int doorNumber)
     {
         doors[doorNumber - 1].gameObject.SetActive(false);
+        foreach (GameObject npc in currentNPC)
+        {
+            npc.gameObject.SetActive(false);
+        }
+        currentNPC.Clear();
+        foreach(GameObject npc in afterEventNPC)
+        {
+            npc.SetActive(true);
+        }
+        currentNPC = afterEventNPC;
+        afterEventNPC.Clear();
     }
 }
