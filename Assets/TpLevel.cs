f87295274ba13916a91f4a1ea08d03041e882493
Néstor <nestcano2971@gmail.com>
2022-09-15 08:57:30 -0500

    TP re revisado

---
+++ Assets/TpLevel.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TpLevel : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene("level 1 test");
+        }
+    }
+
+}
