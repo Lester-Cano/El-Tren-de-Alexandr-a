9838b82abb9cfe0fc97213281ab254495247365f
Alejandro Quiceno Mejía <alejo.123.ks@gmail.com>
2022-10-04 00:33:03 -0500

    first feedback to the boxes

--- Assets/Scripts/Character Controller/Pickup and throw/ObjectPickUp.cs
+++ Assets/Scripts/Character Controller/Pickup and throw/ObjectPickUp.cs
@@ -2,9 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
+using MoreMountains.Feedbacks;
 
 public class ObjectPickUp : MonoBehaviour
 {
     public Rigidbody rb;
     public BoxCollider bcoll;
+    MMF_Player MMF_Player;
+    private void Awake()
+    {
+        MMF_Player = GetComponent<MMF_Player>();
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            Debug.Log("tal");
+            MMF_Player.PlayFeedbacks();
+            var mmfPar = MMF_Player.FindObjectOfType<MMFeedbackParticlesInstantiation>();
+        }
+    }
 }
