3b9409c68f8aa47d51ea3c8e52d81333201117b5
Alejandro Quiceno Mejía <alejo.123.ks@gmail.com>
2022-10-06 17:04:24 -0500

    changes to the transition

--- Assets/Scripts/GameManager/PassZone.cs
+++ Assets/Scripts/GameManager/PassZone.cs
@@ -8,33 +8,42 @@ public class PassZone : MonoBehaviour
 {
     [SerializeField] private GameObject pos;
     private GameObject objectToMove;
+    [SerializeField] Vector3 offsetPostion = new Vector3 (0,0,3);
     // transition variables
     [SerializeField] Material transitionMaterial;
+    [SerializeField] private Ease ease;
     float transitionValue;
-    Sequence tranSequence = DOTween.Sequence();
+    Sequence tranSequence;
+    private void Start()
+    {
+        ease = Ease.InOutSine;
+        transitionMaterial = Resources.Load<Material>("Transition_material");
+        transitionMaterial.SetFloat("_transitionAmount", 0);
+        tranSequence = DOTween.Sequence();
+    }
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Player"))
+        if (other.gameObject.CompareTag("Player") && !tranSequence.IsPlaying())
         {
             objectToMove = other.gameObject;
-            StartCoroutine(Teleport());
-
+            Invoke("StartCorrutineDelay", 1.5f);
+            tranSequence.Append(transitionMaterial.DOFloat(1f, "_transitionAmount", 1.5f).SetEase(ease));
+            tranSequence.Append(transitionMaterial.DOFloat(0f, "_transitionAmount", 1.5f).SetEase(ease).SetDelay(2));
         }
     }
-    private void Update()
-    {
-        
-    }
+
     private IEnumerator Teleport()
     {
         if (objectToMove != null)
         {
             pos.SetActive(false);
-            objectToMove.transform.position = pos.transform.position;
-
+            objectToMove.transform.position = pos.transform.position+offsetPostion;
             yield return new WaitForSeconds(2f);
-
             pos.SetActive(true);
         }
+    }
+    private void StartCorrutineDelay()
+    {
+        StartCoroutine(Teleport());
     }
 }
