78c31095d5a6dfe54161ae40cf05c8f84b5380cd
Nicolás <skarddy@gmail.com>
2022-10-17 12:40:36 -0500

    Talk SFX

--- Assets/Scripts/Character Controller/Talk/NPCDialogue.cs
+++ Assets/Scripts/Character Controller/Talk/NPCDialogue.cs
@@ -7,4 +7,16 @@ public class NPCDialogue : MonoBehaviour
     public string[] names;
     public string[] dialogues;
     public List<NPCImage> NPCImages;
+    private SFXManager SFX;
+    [SerializeField] AudioClip TalkClip;
+
+    private void Start() {
+        SFX = FindObjectOfType<SFXManager>();
+    }
+
+    public void OnTalkNPC() {
+        SFX.SFXSource.PlayOneShot(TalkClip);
+    }
+
+
 }
